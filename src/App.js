38bb1c02797f17f6d6d04f71f936f60fb616bdbf
connorbaush <baush.c@northeastern.edu>
2023-07-06 21:50:07 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-                  Welcome to Web Dev!
-                  An update
+                  Hello my dumpling!
         </p>
         <a
           className="App-link"
