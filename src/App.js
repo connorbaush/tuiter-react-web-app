a7b531931b6c88275aa099708e977707aea34536
connorbaush <baush.c@northeastern.edu>
2023-07-06 21:54:20 -0400

    adding

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-                  Hello my dumpling!
-                  I love you very much!
+                  Welcome to Web Dev!
         </p>
         <a
           className="App-link"
