ca3663f968c55307c6fc5ef29e0de086366a1bb2
connorbaush <baush.c@northeastern.edu>
2023-07-22 20:50:28 -0400

    small change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
-import { HashRouter } from "react-router-dom";
 
 function App() {
     return (
-        <HashRouter>
-            <div className="container">
-                <HelloWorld />
-                <Labs />
-                <Tuiter />
-            </div>
-        </HashRouter>
+        <div className="container">
+            <HelloWorld />
+            <Labs />
+            <Tuiter />
+            Making a change
+        </div>
     );
 }
 export default App;
