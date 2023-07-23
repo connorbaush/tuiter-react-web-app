f1cf2c6cbb5ad321c718e5a62df676bfdc0589c4
connorbaush <baush.c@northeastern.edu>
2023-07-22 22:52:10 -0400

    revert

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
-import { HashRouter } from "react-router-dom";
-import { Routes, Route } from "react-router";
 
 function App() {
     return (
-        <HashRouter>
-                <Routes>
-                   
-                    <Route path="/labs/*" element={<Labs />} />
-                    <Route path="/hello" element={<HelloWorld />} />
-                    <Route path="/tuiter/*" element={<Tuiter />} />
-                </Routes>
-        </HashRouter>
+        <div className="container">
+            <HelloWorld />
+            <Labs />
+            <Tuiter />
+        </div>
     );
 }
 export default App;
