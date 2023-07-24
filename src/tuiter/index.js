1ccaab5c8255270cbb818e354418a5c2d75efb7b
connorbaush <baush.c@northeastern.edu>
2023-07-24 17:33:23 -0400

    almost done

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,11 +1,39 @@
-import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import HomeScreen from "./home-screen";
+import ExploreScreen from "./explore-screen";
+import BookmarksScreen from "./bookmarks-screen";
+import ProfileScreen from "./profile-screen";
+import WhoToFollowList
+    from "./who-to-follow-list/";
+import TuitSummaryList from "./tuit-summary-list";
+
+
 function Tuiter() {
     return (
         <div>
             <Nav />
-            <h1>Tuiter</h1>
+            <div className="row">
+                <div className="col-2">
+                    <NavigationSidebar />
+                </div>
+                <div className="col-7">
+                    <Routes>
+                        <Route path="/home" element={<HomeScreen />} />
+                        <Route path="/explore" element={<ExploreScreen />} />
+                        <Route path="/bookmarks" element={<BookmarksScreen />} />
+                        <Route path="/profile" element={<ProfileScreen />} />
+                    </Routes>
+                    <TuitSummaryList />
+
+                </div>
+                <div className="col-3">
+                    <h1>Who to follow</h1>
+                    <WhoToFollowList />
+                </div>
+            </div>
         </div>
-    )
-};
-export default Tuiter
+    );
+}
+export default Tuiter;
