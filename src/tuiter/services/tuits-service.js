07338d2940519a5b1ab4e1f4718b03cc16119dc5
connorbaush <baush.c@northeastern.edu>
2023-08-10 00:21:51 -0400

    just in case

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = process.env.REACT_APP_SERVER_API_URL;
+const TUITS_API = "https://tuiter-ac7s.onrender.com/api/tuits";
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
